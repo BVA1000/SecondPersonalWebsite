1971694800a0f1ba16eed0d35cdcded9ea4a9e95
BenVonAchen <bennyvonachen@gmail.com>
2021-08-19 12:54:53 -0600

    First Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+import { AboutComponent } from './about/about.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
+import { GoalsComponent } from './goals/goals.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", redirectTo: "home", pathMatch: "full" },
+  { path: "home", component: HomepageComponent },
+  { path: "about", component: AboutComponent },
+  { path: "projects", component: ProjectsComponent },
+  { path: "goals", component: GoalsComponent },
+  { path: "contact", component: ContactComponent },
+  { path: "*", component: HomepageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { NavComponent } from './nav/nav.component';
+import { AboutComponent } from './about/about.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
+import { GoalsComponent } from './goals/goals.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    NavComponent,
+    AboutComponent,
+    ProjectsComponent,
+    ContactComponent,
+    GoalsComponent
   ],
   imports: [
     BrowserModule,
